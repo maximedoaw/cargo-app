d2f1daa328f23eaed8a353f006205033c086100d
maximedoaw <maximedoaw204@gmail.com>
2024-12-06 03:19:51 +0100

    ajout du bouton whatapp

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,11 @@ import FreightStats from "./components/FreightStats";
 import NavBar from "./components/NavBar";
 import SpecialServices from "./components/SpecialServices";
 import StaticImageSection from "./components/StaticImageSection";
+import { FaWhatsapp } from "react-icons/fa";
+
 export default function App() {
   return (
-    <div className="">
+    <div className="relative">
       <NavBar />
       <StaticImageSection />
       <Cards />
@@ -15,6 +17,17 @@ export default function App() {
       <SpecialServices />
       <FreightStats />
       <Footer />
+
+      {/* Bouton WhatsApp fixe */}
+      <a
+        href="https://wa.me/677271237" // Remplacez par votre numéro WhatsApp
+        target="_blank"
+        rel="noopener noreferrer"
+        className="fixed bottom-4 right-4 bg-[#25D366] text-white p-4 rounded-full shadow-lg hover:opacity-90 transition-all duration-300"
+        title="Contactez-nous sur WhatsApp"
+      >
+        <FaWhatsapp size={28} />
+      </a>
     </div>
-  )
+  );
 }
